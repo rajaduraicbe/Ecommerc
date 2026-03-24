fcc914c224457580536ccc3ae734da9e4a670ba6
shibu-pix <shibu.pixalive@gmail.com>
2026-03-24 18:27:58 +0530

    Hero section by shibu

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
+import Hero from './Components/Landing/Hero'
+
 
 
 const App = () => {
   return (
     <div>
      <Header />
+     <Hero/>
      <Footer />
      
     </div>
