b2d6e2b5d58e7822c8310d01eaf3930ae7bd49e2
shibu-pix <shibu.pixalive@gmail.com>
2026-03-25 16:22:05 +0530

    rename file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
-import Hero from './Components/Landing/Hero'
+import Hero from './Components/Landingpage/Hero'
 
 
 
